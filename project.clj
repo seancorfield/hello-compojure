13e46cf85da49e25fbd113f84dfa9706e0a92742
Sean Corfield <sean@corfield.org>
2020-08-09 19:22:44 -0700

    Remove redundant Ring/Jetty dep

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring "1.8.1"]
-                 [ring/ring-jetty-adapter "1.8.1"]
+                 ;[ring/ring-jetty-adapter "1.8.1"]
                  [ring/ring-defaults "0.3.2"]]
   :main seancorfield.hello-compojure.handler
   :ring {:handler seancorfield.hello-compojure.handler/app}
